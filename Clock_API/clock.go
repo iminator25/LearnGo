e8e3d9e17de57c1567bca4569ae61f92140edb08
Imran Adamjee <imey2597@gmail.com>
2020-11-10 23:04:11 -0600

    clock coming out with second hand

--- Clock_API/clock.go
+++ Clock_API/clock.go
@@ -11,9 +11,17 @@ type Point struct {
 	Y float64
 }
 
+const SecondHandLength = 90
+const clockCenterX = 150
+const clockCenterY = 150
+
 // unit vector of the second hand at time 't'
 func SecondHand(t time.Time) Point {
-	return Point{150, 60}
+	p := secondHandPoint(t)
+	p = Point{p.X * SecondHandLength, p.Y * SecondHandLength}
+	p = Point{p.X, -p.Y}
+	p = Point{p.X + clockCenterX, p.Y + clockCenterY}
+	return p
 }
 
 func secondsInRadians(t time.Time) float64 {
